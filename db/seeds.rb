f6549f50fa77ad2611b0532bef338549b829b703
sebastian spadea <spadea.sebastian@gmail.com>
2021-03-17 15:48:22 -0300

    Se agrego la gema devise, se creo la tabla de usuarios y la seed con el nuevo usuario

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,9 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+#   
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Creating a new user"
+new_user = User.create!(email: "new_user_example@example.com",password: "123456" )
+puts "New user created"
